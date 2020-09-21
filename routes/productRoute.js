04d390b65331ee68ef8402e4bd1abf5731a62b81
Alexandre Previ <alexandre.previ@gmail.com>
2020-09-21 13:02:01 +0200

    modified .en.sample

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,13 +1,12 @@
-const productController = require('../controllers/product.controller')
+const productController = require("../controllers/product.controller");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', productController.getProducts);
-router.get('/:id', productController.getProduct);
-router.post('/', productController.createProduct);
-router.patch('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
-
+router.get("/", productController.getProducts);
+router.get("/:id", productController.getProduct);
+router.post("/", productController.createProduct);
+router.patch("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
